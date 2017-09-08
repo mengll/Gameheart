4a5058ec00f74d6c960c359b76f8911ed71ca3b7
root <root@localhost.localdomain>
2017-09-08 00:33:13 -0700

    add back data type

--- controller/GameHeart.go
+++ controller/GameHeart.go
@@ -1,68 +1,121 @@
 package controller
 
-```
-Jump This function is for get the GameHeart message
-Logs This function is save the request log
-Downup This function is delete user when you leave
-```
-type GameHeart interface {
+import (
+	"fmt"
+	"net/http"
+)
+
+///*
+//Jump This function is for get the GameHeart message
+//Logs This function is save the request log
+//Downup This function is delete user when you leave
+//```
+type GameHeartInterface interface {
 	Jump()
 	Logs()
 	Downup()
 }
 
-
-```
- This is config the GameHeart Message type
- Date string Heart jump time
- Ucid The game ucid anfanapi table
- Pid  the game id which game 
- Rid  the channel 
- Imei   android is imei ios is idfa 
-```
+//```
+// This is config the GameHeart Message type
+// Date string Heart jump time
+// Ucid The game ucid anfanapi table
+// Pid  the game id which game
+// Rid  the channel
+// Imei   android is imei ios is idfa
+//```
 
 type HeartMessageType struct {
-	Date string "json:'date'",
-	Ucid string "json:'ucid'",
-	Pid  string "json:'pid'" ,
-	Rid  string "json:'rid'" ,
-	Imei string "json:'imei'"
+	Date string `json:"date"`
+	Ucid string `json:"ucid"`
+	Pid  string `json:"pid"`
+	Rid  string `json:"rid"`
+	Imei string `json:"imei"`
 }
 
-```
-  GameHeart is handle 
+//```
+//  GameHeart is handle
 
-```
-
-type  GameHeart struct {
-	
+type mess struct {
+	CloseTime int    `json:"close_time"`
+	Content   string `json:"content"`
 }
 
-
-```
- Get the oheart jump Data
-```
-
-func (self *GameHeart) Jump(msg *HeartMessageType){
-	
+type Dat struct {
+	Broadcast []mess `json:"broadcast"`
+	OnlineNum int    `json:"online_num"`
+	WinRate   int    `json:"win_rate"`
 }
 
-
-```
- The Log handle
-```
-
-func (self *GameHeart)Logs(msg *HeartMessageType){
-	
+type messData struct {
+	Code int    `json:"code"`
+	Data Dat    `json:"data"`
+	Msg  string `json:"msg"`
 }
 
-```
-   Downup The player is leave
-```
-func (self *GameHeart)Downup(msg *HeartMessageType){
-	
+//the back data type
+
+func hear(w http.ResponseWriter, r *http.Request) {
+
+	method := r.Method
+	if strings.ToLower(method) != "post" {
+		con_m := mess{}
+		con_m.CloseTime = 3
+		con_m.Content = "colose Dat"
+		hj := []mess{con_m}
+
+		metdat := Dat{}
+		metdat.OnlineNum = 2
+		metdat.WinRate = 2
+		metdat.Broadcast = hj
+		dat := messData{}
+		dat.Code = 1
+		dat.Msg = "error"
+		dat.Data = metdat
+
+		cont, err := json.Marshal(dat)
+		if err != nil {
+			return
+		}
+
+		w.Write(cont)
+		fmt.Println("one")
+		return
+	}
 }
 
+//```
 
+type GameHeart struct {
+}
 
+//```
+// Get the oheart jump Data
+//```
 
+func (self *GameHeart) Jump(msg *HeartMessageType) {
+
+}
+
+//```
+// The Log handle
+//```
+
+func (self *GameHeart) Logs(msg *HeartMessageType) {
+
+}
+
+//```
+//   Downup The player is leave
+//```
+func (self *GameHeart) Downup(msg *HeartMessageType) {
+
+}
+
+//```
+//GameHeart http request
+//```
+func GameHeartRequest(w http.ResponseWriter, r *http.Request) {
+	//create The int game data
+	fmt.Println("This is Game heart")
+}
--- main.go
+++ main.go
@@ -2,21 +2,23 @@
 package main
 
 import (
+	"Gameheart/controller"
+	"flag"
 	"fmt"
 	"net/http"
 	"runtime"
 )
 
 //the app init
+var port string
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
-
 func main() {
+	flag.StringVar(&port, "port", ":8080", "the server port")
 	fmt.Println("Hello World!")
-	servbe ;= http.HandleFunc("/heart",)
-	
-
+	http.HandleFunc("/heart", controller.GameHeartRequest)
+	http.ListenAndServe(port, nil)
 }
